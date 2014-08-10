066a13fe19d9f2438de4d4d61719646a5196b465
Vincent Gu <g@v-io.co>
2014-08-11 03:02:56 +0800

    remove useless comments

--- guahao/main.go
+++ guahao/main.go
@@ -32,49 +32,6 @@ func parseArgs() {
 	flag.Parse()
 }
 
-func loadZjol() {
-	// // check session-id file
-	// sessionFile, err := os.Open(sessionFPath)
-	// if err != nil {
-	// 	sessionFile, err = os.Create(sessionFPath)
-	// 	if err != nil {
-	// 		glog.Fatalln(err)
-	// 	}
-	// 	defer sessionFile.Close()
-	// 	return realLogin()
-	// }
-	// defer sessionFile.Close()
-
-	// reader := bufio.NewReader(sessionFile)
-	// session.SessionId, err = reader.ReadString('\n')
-	// if err == nil {
-	// 	session.SessionId = session.SessionId[:len(session.SessionId)-1]
-	// } else if err != nil {
-	// 	glog.Warningf("only session id stored in cache file, redirect to realLogin()\n")
-	// 	return realLogin()
-	// }
-	// session.UserId, err = reader.ReadString('\n')
-	// if err == nil {
-	// 	session.UserId = session.UserId[:len(session.UserId)-1]
-	// } else if err != nil && err != io.EOF {
-	// 	glog.Infof("error occured, redirect to realLogin()\n")
-	// 	return realLogin()
-	// }
-
-	// client := &http.Client{Jar: jar}
-	// sidCookie := &http.Cookie{
-	// 	Domain: "guahao.zjol.com.cn", Path: "/",
-	// 	Name: "ASP.NET_SessionId", Value: session.SessionId,
-	// 	HttpOnly: true, MaxAge: 0}
-	// uidCookie := &http.Cookie{
-	// 	Domain: "guahao.zjol.com.cn", Path: "/",
-	// 	Name: "UserId", Value: session.UserId,
-	// 	HttpOnly: true, MaxAge: 0}
-	// u, err := url.Parse(domUrl)
-	// client.Jar.SetCookies(u, []*http.Cookie{sidCookie, uidCookie})
-
-}
-
 func main() {
 	parseArgs()
 	// startServer()
