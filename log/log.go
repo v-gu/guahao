3d0425794b49864f1b64920ce98d3250267e91b0
Vincent Gu <g@v-io.co>
2014-09-04 05:01:53 +0800

    fix log output format issue

--- log/log.go
+++ log/log.go
@@ -19,50 +19,68 @@ type NamedLogger struct {
 
 //
 func (m *NamedLogger) Info(args ...interface{}) {
-	glog.Info(m.Name+": ", args)
+	nargs := []interface{}{m.Name + ": "}
+	nargs = append(nargs, args...)
+	glog.Info(nargs...)
 }
 func (m *NamedLogger) Infoln(args ...interface{}) {
-	glog.Infoln(m.Name+": ", args)
+	nargs := []interface{}{m.Name + ": "}
+	nargs = append(nargs, args...)
+	glog.Infoln(nargs...)
 }
 func (m *NamedLogger) Infof(format string, args ...interface{}) {
-	glog.Infof("%s: "+format, m.Name, args)
+	nargs := []interface{}{m.Name}
+	nargs = append(nargs, args...)
+	glog.Infof("%s: "+format, nargs...)
 }
 
 //
 func (m *NamedLogger) Warning(args ...interface{}) {
-	glog.Warning(m.Name+": ", args)
+	nargs := []interface{}{m.Name + ": "}
+	nargs = append(nargs, args...)
+	glog.Warning(nargs...)
 }
 func (m *NamedLogger) Warningln(args ...interface{}) {
-	glog.Warningln(m.Name+": ", args)
+	nargs := []interface{}{m.Name + ": "}
+	nargs = append(nargs, args...)
+	glog.Warningln(nargs...)
 }
 func (m *NamedLogger) Warningf(format string, args ...interface{}) {
-	glog.Warningf("%s: "+format, args)
+	nargs := []interface{}{m.Name}
+	nargs = append(nargs, args...)
+	glog.Warningf("%s: "+format, nargs...)
 }
 
 //
 func (m *NamedLogger) Error(args ...interface{}) {
-	glog.Error(m.Name+": ", args)
+	nargs := []interface{}{m.Name + ": "}
+	nargs = append(nargs, args...)
+	glog.Error(nargs...)
 }
 func (m *NamedLogger) Errorln(args ...interface{}) {
-	glog.Errorln(m.Name+": ", args)
+	nargs := []interface{}{m.Name + ": "}
+	nargs = append(nargs, args...)
+	glog.Errorln(nargs...)
 }
 func (m *NamedLogger) Errorf(format string, args ...interface{}) {
-	glog.Errorf("%s: "+format, args)
+	nargs := []interface{}{m.Name}
+	nargs = append(nargs, args...)
+	glog.Errorf("%s: "+format, nargs...)
 }
 
 //
 func (m *NamedLogger) Debug(level glog.Level, args ...interface{}) {
 	if glog.V(level) {
-		m.Info(args)
+		m.Info(args...)
 	}
 }
 func (m *NamedLogger) Debugln(level glog.Level, args ...interface{}) {
 	if glog.V(level) {
-		m.Infoln(args)
+		m.Infoln(args...)
 	}
 }
 func (m *NamedLogger) Debugf(level glog.Level, format string, args ...interface{}) {
 	if glog.V(level) {
-		m.Infof(format, args)
+		m.Infof(format, args...)
 	}
 }
